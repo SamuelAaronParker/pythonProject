ca7a29f3a3e39d94fc2bdfdf6a963963360ff08d
SamuelAaronParker <samuel.parker@sky.uk>
2023-02-20 18:52:10 +0000

    Added files and commits for bankingpin.py, test.py and ex10_starter.py - Sam Parker

--- Exercise 10/ex10_starter.py
+++ Exercise 10/ex10_starter.py
@@ -11,6 +11,22 @@ else:
 # Construct a portable wildcard pattern
 pattern = os.path.join(hdir, '*')
 
+path = "../Python challenges/"
+pathf = '../Python challenges/Filefind2.py'
+size = os.path.getsize(pathf)
+
+print("Size (In bytes) of '%s':" % pathf, size)
+print(os.listdir(path))
+
+for filename in os.listdir(path):
+    filepath = os.path.join(path, filename)
+    # print(filepath)
+    if os.path.isfile(filepath) and os.path.getsize(filepath) > 0:
+        print(filename)
+
+pathbname = os.path.basename(filename)
+
+print(pathbname)
 # TODO: Use the glob.glob() function to obtain the list of filenames
 
 # TODO: use os.path.getsize to find each file's size
