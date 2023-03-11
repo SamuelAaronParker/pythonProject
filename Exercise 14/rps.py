d9c48cc0fda0c668b1294c65e76b4d77ed9af286
Sam Parker <samuel.parker@sky.uk>
2023-03-11 13:44:12 +0000

    Added back computer options and gave player choices. Still working on getpass to hide players selection.

--- Exercise 14/rps.py
+++ Exercise 14/rps.py
@@ -1,4 +1,4 @@
-#import getpass
+import random
 from enum import IntEnum
 
 
@@ -15,53 +15,75 @@ victories = {
 }
 
 
-def get_user_selection():
+def get_user_selection(player):
     choices = [f"{action.name}[{action.value}]" for action in Action]
     choices_str = ", ".join(choices)
-    selection = int(input(f"Player 1 enter a choice ({choices_str}): "))
-    action = Action(int(selection))
+    selection = input(f"{player} enter a choice ({choices_str}): ")
+    try:
+        action = Action(int(selection))
+    except ValueError:
+        print("Invalid selection. Please try again.")
+        return get_user_selection(player)
     return action
 
 
-def get_user_selection2():
-    choices = [f"{action.name}[{action.value}]" for action in Action]
-    choices_str = ", ".join(choices)
-    selection = int(input(f"Player 2 enter a choice ({choices_str}): "))
-    action = Action(int(selection))
-    return action
+def get_computer_selection():
+    return random.choice(list(Action))
 
 
-def determine_winner(user_action1, user_action2, score1, score2):
-    defeats = victories[user_action1]
-    if user_action1 == user_action2:
-        print(f"Both players selected {user_action1.name}. It's a tie!")
-    elif user_action2 in defeats:
-        print(f"{user_action1.name} beats {user_action2.name}! Player 1 wins with {user_action1.name}!")
-        score1 += 1
+def determine_winner(user_action, computer_action, player):
+    if user_action == computer_action:
+        print(f"Both players selected {user_action.name}. It's a tie!")
+        return "tie"
+    elif computer_action in victories[user_action]:
+        print(f"{user_action.name} beats {computer_action.name}! {player} wins!")
+        return player
     else:
-        print(f"{user_action2.name} beats {user_action1.name}! Player 2 wins with {user_action1.name}!")
-        score2 += 1
+        print(f"{computer_action.name} beats {user_action.name}! Computer wins!")
+        return "computer"
 
-    return score1, score2
-
-
-score1 = 0
-score2 = 0
 
 while True:
-    try:
-        user_action1 = get_user_selection()
-    except ValueError as e:
-        range_str = f"[0, {len(Action) - 1}]"
-        print(f"Please only select one number between the following{choices_str}")
-        continue
+    mode = input("Select a mode (1 for two-player, 2 for against computer): ")
+    if mode == "1":
+        player1 = input("Enter player 1 name: ")
+        player2 = input("Enter player 2 name: ")
+        score1 = 0
+        score2 = 0
+        while True:
+            user_action1 = get_user_selection(player1)
+            user_action2 = get_user_selection(player2)
+            result = determine_winner(user_action1, user_action2, player1)
+            if result == player1:
+                score1 += 1
+            elif result == player2:
+                score2 += 1
 
-    user_action2 = get_user_selection2()
-    score1, score2 = determine_winner(user_action1, user_action2, score1, score2)
+            print(f"{player1} score: {score1}")
+            print(f"{player2} score: {score2}")
 
-    print(f"Player 1 score: {score1}")
-    print(f"Player 2 score: {score2}")
+            play_again = input("Play again? (y/n): ")
+            if play_again.lower() != "y":
+                break
 
-    play_again = input("Play again? (y/n): ")
-    if play_again.lower() != "y":
-        break
+    elif mode == "2":
+        player = input("Enter player name: ")
+        score = 0
+        compscore = 0
+        while True:
+            user_action = get_user_selection(player)
+            computer_action = get_computer_selection()
+            result = determine_winner(user_action, computer_action, player)
+            if result == player:
+                score += 1
+            elif result == "computer":
+                compscore += 1
+
+            print(f"{player} score: {score}\nComputer score: {compscore}")
+
+            play_again = input("Play again? (y/n): ")
+            if play_again.lower() != "y":
+                break
+
+    else:
+        print("Invalid mode. Please try again.")
